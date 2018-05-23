f1a2d8ad5bafdee6f4ff57cfb65ab11fc20ef999
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-05-23 11:29:50 +0200

    REST

---
+++ src/main/java/pl/training/bank/account/controller/AccountController.java
@@ -0,0 +1,35 @@
+package pl.training.bank.account.controller;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import pl.training.bank.account.dto.AccountDto;
+import pl.training.bank.account.entity.Account;
+import pl.training.bank.account.services.AccountService;
+import pl.training.bank.common.UriBuilder;
+import pl.training.bank.common.mapper.Mapper;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/accounts")
+@RestController
+public class AccountController {
+
+    @NonNull
+    private Mapper mapper;
+    @NonNull
+    private AccountService accountService;
+    private UriBuilder uriBuilder = new UriBuilder();
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity createAccount() {
+        Account account = accountService.createAccount();
+        URI uri = uriBuilder.requestUriWithId(account.getId());
+        return ResponseEntity.created(uri).body(mapper.map(account, AccountDto.class));
+    }
+
+}
--- src/main/java/pl/training/bank/config/Mvc.java
+++ src/main/java/pl/training/bank/config/Mvc.java
@@ -2,6 +2,7 @@ package pl.training.bank.config;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pl.training.bank.common.mapper.Mapper;
 import pl.training.bank.common.mapper.ModelMapperAdapter;
 
+@ComponentScan(basePackages = "pl.training.bank")
 @EnableWebMvc
 @Import(Bank.class)
 @Configuration
