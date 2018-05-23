bc18e40492bc318be78ed30ec87cccd62f93d712
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-05-23 12:25:19 +0200

    REST

--- src/main/java/pl/training/bank/config/Mvc.java
+++ src/main/java/pl/training/bank/config/Mvc.java
@@ -7,10 +7,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pl.training.bank.common.mapper.Mapper;
 import pl.training.bank.common.mapper.ModelMapperAdapter;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
+@EnableSwagger2
 @ComponentScan(basePackages = "pl.training.bank")
 @EnableWebMvc
 @Import(Bank.class)
@@ -28,6 +35,22 @@ public class Mvc implements WebMvcConfigurer {
     @Bean
     public Mapper mapper() {
         return new ModelMapperAdapter();
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(SWAGGER_2)
+                .select()
+                .apis(basePackage("pl.training.bank"))
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 }
