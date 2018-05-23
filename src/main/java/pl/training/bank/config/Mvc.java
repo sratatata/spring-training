ec922f6a8a590c24c380524e53a7b7b5763e26bd
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-05-23 14:24:30 +0200

    Spring Boot

--- src/main/java/pl/training/bank/config/Mvc.java
+++ src/main/java/pl/training/bank/config/Mvc.java
@@ -1,13 +1,7 @@
 package pl.training.bank.config;
 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pl.training.bank.common.mapper.Mapper;
 import pl.training.bank.common.mapper.ModelMapperAdapter;
@@ -18,19 +12,8 @@ import static springfox.documentation.builders.RequestHandlerSelectors.basePacka
 import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 @EnableSwagger2
-@ComponentScan(basePackages = "pl.training.bank")
-@EnableWebMvc
-@Import(Bank.class)
 @Configuration
 public class Mvc implements WebMvcConfigurer {
-
-    @Bean
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("text");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
-    }
 
     @Bean
     public Mapper mapper() {
@@ -43,14 +26,6 @@ public class Mvc implements WebMvcConfigurer {
                 .select()
                 .apis(basePackage("pl.training.bank"))
                 .build();
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 }
