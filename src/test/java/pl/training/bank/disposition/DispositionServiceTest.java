e1af734acb9a9e04c93f51f8180d53e191129de3
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-05-23 14:14:16 +0200

    Spring Boot

--- src/test/java/pl/training/bank/disposition/DispositionServiceTest.java
+++ src/test/java/pl/training/bank/disposition/DispositionServiceTest.java
@@ -1,8 +1,7 @@
 package pl.training.bank.disposition;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 import pl.training.bank.account.entity.Account;
 import pl.training.bank.account.services.AccountNotFoundException;
 import pl.training.bank.account.repository.AccountRepository;
@@ -16,10 +15,9 @@ import java.util.Optional;
 
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.singletonMap;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
-class DispositionServiceTest {
+public class DispositionServiceTest {
 
     private Operation operation = mock(Operation.class);
     private Disposition disposition = new Disposition("00000000000000000000000001", 10_000, "deposit");
@@ -27,37 +25,33 @@ class DispositionServiceTest {
     private Account account = mock(Account.class);
     private AccountRepository accountRepository = mock(AccountRepository.class);
 
-    @BeforeEach
-    void setUp() {
+    @Before
+    public void setUp() {
         when(accountRepository.getByNumber(anyString())).thenReturn(Optional.of(account));
     }
 
-    @DisplayName("Should throw exception when account not found")
-    @Test
-    void shouldThrowExceptionWhenAccountNotFound() {
+    @Test(expected = AccountNotFoundException.class)
+    public void shouldThrowExceptionWhenAccountNotFound() {
         DispositionService dispositionService = new DispositionService(mock(AccountRepository.class), operations);
-        assertThrows(AccountNotFoundException.class, () -> dispositionService.process(disposition));
+        dispositionService.process(disposition);
     }
 
-    @DisplayName("Should throw exception when operation is unknown")
-    @Test
-    void shouldThrowExceptionWhenOperationIsUnknown() {
+    @Test(expected = UnknownOperationException.class)
+    public void shouldThrowExceptionWhenOperationIsUnknown() {
         DispositionService dispositionService = new DispositionService(accountRepository, emptyMap());
-        assertThrows(UnknownOperationException.class, () -> dispositionService.process(disposition));
+        dispositionService.process(disposition);
     }
 
-    @DisplayName("Should execute operation")
     @Test
-    void shouldExecuteOperation() {
+    public void shouldExecuteOperation() {
         DispositionService dispositionService = new DispositionService(accountRepository, operations);
         dispositionService.process(disposition);
         verify(operation).execute(account, disposition.getFunds());
     }
 
 
-    @DisplayName("Should update account")
     @Test
-    void shouldUpdateAccount() {
+    public void shouldUpdateAccount() {
         DispositionService dispositionService = new DispositionService(accountRepository, operations);
         dispositionService.process(disposition);
         verify(accountRepository).save(account);
