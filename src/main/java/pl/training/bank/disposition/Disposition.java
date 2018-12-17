722e4965b7c41e83064e6e6858581bd3e7cc1ab9
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-12-17 16:13:45 +0100

    Clean project

--- src/main/java/pl/training/bank/disposition/Disposition.java
+++ src/main/java/pl/training/bank/disposition/Disposition.java
@@ -4,12 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Disposition {
 
+    @Pattern(regexp = "\\d{26}")
+    @NotNull
     private String accountNumber;
+    @Min(1)
     private long funds;
     private String operationName;
 
