cdb3d6f7b96ceec863e2e74c8b7c3f7f1bb94396
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-12-18 17:50:02 +0100

    Validator

--- src/main/java/pl/training/bank/account/IncrementalAccountNumberGenerator.java
+++ src/main/java/pl/training/bank/account/IncrementalAccountNumberGenerator.java
@@ -2,6 +2,8 @@ package pl.training.bank.account;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import static java.lang.Long.parseLong;
+
 public class IncrementalAccountNumberGenerator implements AccountNumberGenerator {
 
     private static final String ACCOUNT_NUMBER_FORMAT = "%026d";
@@ -11,6 +13,12 @@ public class IncrementalAccountNumberGenerator implements AccountNumberGenerator
     @Override
     public String next() {
         return String.format(ACCOUNT_NUMBER_FORMAT, counter.incrementAndGet());
+    }
+
+    public void setCurrentAccountNumber(String accountNumber) {
+        if (accountNumber != null) {
+            counter = new AtomicLong(parseLong(accountNumber));
+        }
     }
 
 }
