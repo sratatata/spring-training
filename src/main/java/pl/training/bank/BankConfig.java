a18d50ca9f56c5fd909f3495d47370de7b77d9c0
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2019-02-25 15:21:36 +0100

    Java 11

--- src/main/java/pl/training/bank/BankConfig.java
+++
@@ -1,9 +0,0 @@
-package pl.training.bank;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-@EnableAspectJAutoProxy
-@Configuration
-public class BankConfig {
-}
