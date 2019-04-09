13246f63a598017e214c2cccc4a0cd0eaadec288
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2019-04-09 10:45:38 +0200

    jdbc

--- src/main/java/pl/training/bank/BankConfig.java
+++ src/main/java/pl/training/bank/BankConfig.java
@@ -1,9 +1,28 @@
 package pl.training.bank;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
+import javax.sql.DataSource;
+
+@PropertySource("classpath:jdbc.properties")
 @EnableAspectJAutoProxy
 @Configuration
 public class BankConfig {
+
+    @Bean
+    public DataSource dataSource(Environment environment) {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setUsername(environment.getProperty("database.username"));
+        dataSource.setPassword(environment.getProperty("datasource.password"));
+        dataSource.setJdbcUrl(environment.getProperty("datasource.url"));
+        dataSource.setDriverClassName(environment.getProperty("database.driver"));
+        return dataSource;
+    }
+
+
 }
