5f53917bae281de76d5e4abad438c92846ef6d45
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2019-04-11 06:30:35 +0200

    jdbc

--- src/main/java/pl/training/bank/MvcConfig.java
+++ src/main/java/pl/training/bank/MvcConfig.java
@@ -7,11 +7,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import pl.training.bank.common.mapper.Mapper;
+import pl.training.bank.common.mapper.ModelMapperAdapter;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ResourceBundle;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 @ComponentScan(basePackages = "pl.training.bank")
+@EnableSwagger2
 @EnableWebMvc
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -20,6 +27,7 @@ public class MvcConfig implements WebMvcConfigurer {
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("exceptions");
+        messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
 
@@ -29,6 +37,28 @@ public class MvcConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowedOrigins("http://localhost:4200");
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(SWAGGER_2)
+                .select()
+                .apis(basePackage("pl.training.bank"))
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+
+    @Bean
+    public Mapper mapper() {
+        return new ModelMapperAdapter();
     }
 
 }
